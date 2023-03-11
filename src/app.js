ad296151bd411c4aa5d23839169c97de13da98de
CM <cmonterrosad@hotmail.com>
2023-03-11 17:16:24 -0600

    question 4 post /jobs/id/pay

--- src/app.js
+++ src/app.js
@@ -25,32 +25,75 @@ app.get("/contracts", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
   const contract = await Contract.findAll({
     where: {
-    [Op.or]:[
-        {ClientId:req.profile.id},
-        {ContractorId:req.profile.id},
-    ],
-    status:{[Op.not]:'terminated'}
+      [Op.or]: [{ ClientId: req.profile.id }, { ContractorId: req.profile.id }],
+      status: { [Op.not]: "terminated" },
     },
   });
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
 app.get("/jobs/unpaid", getProfile, async (req, res) => {
-    const { Job } = req.app.get("models");
-    const { Contract } = req.app.get("models");
-    const jobs = await Job.findAll({
-      include:{model:Contract,required:true,where:{
-        [Op.or]:[
-            {ClientId:req.profile.id},
-            {ContractorId:req.profile.id},
+  const { Job } = req.app.get("models");
+  const { Contract } = req.app.get("models");
+  const jobs = await Job.findAll({
+    include: {
+      model: Contract,
+      required: true,
+      where: {
+        [Op.or]: [
+          { ClientId: req.profile.id },
+          { ContractorId: req.profile.id },
         ],
-        status:{[Op.not]:'terminated'}
-        }},
-      where:{
-        paid:{[Op.or]:[{[Op.eq]:null},{[Op.eq]:false}]}
-      }
-    });
-    if (!jobs) return res.status(404).end();
-    res.json(jobs);
+        status: { [Op.not]: "terminated" },
+      },
+    },
+    where: {
+      paid: { [Op.or]: [{ [Op.eq]: null }, { [Op.eq]: false }] },
+    },
   });
+  if (!jobs) return res.status(404).end();
+  res.json(jobs);
+});
+app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
+  const { Job } = req.app.get("models");
+  const { Contract } = req.app.get("models");
+  const { job_id } = req.params;
+  const transaction = await sequelize.transaction();
+  let job;
+  try{
+  job = await Job.findOne({
+    include: {
+      model: Contract,
+      required: true,
+      where: { status: { [Op.not]: "terminated" } },
+    },
+    where: {
+      id: job_id,
+    },
+    lock:true,
+    transaction
+  });
+} catch (err){
+    await transaction.rollback();
+    return next(err);
+}
+  if (!job) return res.status(404).end();
+  if(req.profile.balance >= job.price ){
+    job.Contract.balance = req.profile.balance;
+    try{
+        await job.save({transaction})
+    } catch(err){
+        await transaction.rollback();
+        return res.status(404).end();
+    }
+    await transaction.commit();
+
+    const profile = req.profile;
+    profile.balance = 0
+    await profile.save({profile});
+    
+  }
+  
+  res.json(job);
+});
 module.exports = app;
