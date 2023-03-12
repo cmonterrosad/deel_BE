b3f37ab3e316631fafa86af29a85fce8d7a061fc
CM <cmonterrosad@hotmail.com>
2023-03-11 18:33:34 -0600

    6th endpoint

--- src/app.js
+++ src/app.js
@@ -60,40 +60,142 @@ app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
   const { job_id } = req.params;
   const transaction = await sequelize.transaction();
   let job;
-  try{
-  job = await Job.findOne({
-    include: {
-      model: Contract,
-      required: true,
-      where: { status: { [Op.not]: "terminated" } },
-    },
-    where: {
-      id: job_id,
-    },
-    lock:true,
-    transaction
-  });
-} catch (err){
+  try {
+    job = await Job.findOne({
+      include: {
+        model: Contract,
+        required: true,
+        where: { status: { [Op.not]: "terminated" } },
+      },
+      where: {
+        id: job_id,
+      },
+      lock: true,
+      transaction,
+    });
+  } catch (err) {
     await transaction.rollback();
     return next(err);
-}
+  }
   if (!job) return res.status(404).end();
-  if(req.profile.balance >= job.price ){
+  if (req.profile.balance >= job.price) {
     job.Contract.balance = req.profile.balance;
-    try{
-        await job.save({transaction})
-    } catch(err){
-        await transaction.rollback();
-        return res.status(404).end();
+    try {
+      await job.save({ transaction });
+    } catch (err) {
+      await transaction.rollback();
+      return res.status(404).end();
     }
     await transaction.commit();
 
     const profile = req.profile;
-    profile.balance = 0
-    await profile.save({profile});
-    
+    profile.balance = 0;
+    await profile.save({ profile });
   }
-  
+
   res.json(job);
+});
+
+app.post("/balances/deposit/:userId", getProfile, async (req, res) => {
+  const { Job } = req.app.get("models");
+  const { Contract } = req.app.get("models");
+  const { job_id } = req.params;
+  console.log(req.params);
+  const jobsTotalBalance = await Job.findOne({
+    include: {
+      model: Contract,
+      required: true,
+      where: {
+        [Op.or]: [
+          { ClientId: req.profile.id },
+          { ContractorId: req.profile.id },
+        ],
+        status: { [Op.not]: "terminated" },
+      },
+    },
+    attributes: [[sequelize.fn("SUM", sequelize.col("price")), "totalJobs"]],
+    where: {
+      paid: { [Op.or]: [{ [Op.eq]: null }, { [Op.eq]: false }] },
+    },
+  });
+  if (!jobsTotalBalance) return res.status(404).end();
+  console.log(deposit);
+  const {
+    dataValues: { totalJobs },
+  } = jobsTotalBalance;
+
+  if (deposit > totalJobs * 1.25) {
+    return res
+      .status(406)
+      .json({ error: "Deposit amount is more that 25% of total jobs to pay" })
+      .end();
+  }
+  const profile = req.profile;
+  profile.balance = deposit;
+  await profile.save({ profile });
+  res.json(jobsTotalBalance).end();
+});
+
+app.get("/best-profession", async (req, res) => {
+    const { Job } = req.app.get("models");
+    const { Contract } = req.app.get("models");
+    const {Profile} = req.app.get('models')
+    const { start,end } = req.query;
+    console.log(start);
+    console.log(end);
+    const jobsTotalBalance = await Job.findAll({
+      include: {
+        model: Contract,
+        required: true,
+        where: {
+          status: { [Op.not]: "terminated" },
+        },
+        include:{
+          model: Profile,
+          as:'Client',
+          required: true
+        },
+        include:{
+            model: Profile,
+            as:'Contractor',
+            required: true
+          },
+      },
+      group: "profession",
+      attributes: [[sequelize.fn("SUM", sequelize.col("price")), "totalJobs"]],
+      where: {
+        paid:  { [Op.eq]: true },
+        paymentDate: {[Op.between]: [start,end]}
+  
+      },
+    });
+    if (!jobsTotalBalance) return res.status(404).end();
+    res.json(jobsTotalBalance).end();
+  });
+
+app.get("/admin/best-profession?start=<date>&end=<date>", async (req, res) => {
+  const { Job } = req.app.get("models");
+  const { Contract } = req.app.get("models");
+  const {Profile} = req.app.get('models')
+  const { deposit } = req.body;
+  const jobsTotalBalance = await Job.findOne({
+    include: {
+      model: Contract,
+      required: true,
+      where: {
+        status: { [Op.not]: "terminated" },
+      },
+      include:{
+        model: Profile,
+        required: true
+      },
+    },
+    where: {
+      paid: {  [Op.eq]: true },
+
+    },
+  });
+  if (!jobsTotalBalance) return res.status(404).end();
+  res.json(jobsTotalBalance).end();
 });
 module.exports = app;
